c23cd85601b8b44baa79948fd3b3eb8ee1a9799e
TheBotlyNoob <jj@jj.is-a.dev>
2021-12-10 23:46:46 -0500

    move to CLI

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,66 @@
 // Trying to decode and encode a caesar cipher with a GUI
 
 // #![windows_subsystem = "windows"]
-use iced::{
-  executor, widget, Application, Clipboard, Column, Command, Element, Result, Settings, TextInput,
-};
 use spellbound::Checker;
+use std::io::Result;
 
-fn main() -> Result {
-  App::run(Settings::default())
+fn main() -> Result<()> {
+  // let mut spell_checker = Checker::new();
+
+  // println!(
+  //   "{:#?}",
+  //   spell_checker
+  //     .check(&get_input("Type A Sentence:"))
+  //     .map(|error| error.text().to_owned())
+  //     .collect::<Vec<String>>()
+  // );
+
+  println!(
+    r#" 
+     _____                                  _____  _         _                  ______                         _                        _____                          _             
+    /  __ \                                /  __ \(_)       | |                 |  _  \                       | |               ___    |  ___|                        | |            
+    | /  \/  __ _   ___  ___   __ _  _ __  | /  \/ _  _ __  | |__    ___  _ __  | | | |  ___   ___   ___    __| |  ___  _ __   ( _ )   | |__   _ __    ___   ___    __| |  ___  _ __ 
+    | |     / _` | / _ \/ __| / _` || '__| | |    | || '_ \ | '_ \  / _ \| '__| | | | | / _ \ / __| / _ \  / _` | / _ \| '__|  / _ \/\ |  __| | '_ \  / __| / _ \  / _` | / _ \| '__|
+    | \__/\| (_| ||  __/\__ \| (_| || |    | \__/\| || |_) || | | ||  __/| |    | |/ / |  __/| (__ | (_) || (_| ||  __/| |    | (_>  < | |___ | | | || (__ | (_) || (_| ||  __/| |   
+     \____/ \__,_| \___||___/ \__,_||_|     \____/|_|| .__/ |_| |_| \___||_|    |___/   \___| \___| \___/  \__,_| \___||_|     \___/\/ \____/ |_| |_| \___| \___/  \__,_| \___||_|   
+                                                     | |                                                                                                                             
+                                                     |_|                                                                                                                             "#
+  );
+
+  let menu_options = vec!["Decode", "Encode"];
+
+  for (i, item) in menu_options.iter().enumerate() {
+    println!("{}: {}", i + 1, item);
+  }
+
+  let menu_option = get_int(&format!("Choose An Option (1-{}): ", menu_options.len()));
+
+  println!("{}", menu_option);
+
+  Ok(())
 }
 
-#[derive(Default)]
-struct App {
-  key_input_state: widget::text_input::State,
-  key: String,
+fn get_string(prompt: &str) -> String {
+  use io::Write;
+  use std::io;
+
+  print!("{}", prompt);
+
+  io::stdout().flush().ok();
+
+  let mut input = String::new();
+
+  io::stdin().read_line(&mut input).ok();
+
+  input.trim().to_owned()
 }
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-  Encrypt(u8),
-  Decrypt(u8),
-  BruteForce,
-  Nothing,
-}
+fn get_int(prompt: &str) -> i128 {
+  let mut int = get_string(prompt).parse::<i128>();
 
-impl Application for App {
-  type Executor = executor::Default;
-  type Flags = ();
-  type Message = Message;
-
-  fn new(_flags: ()) -> (Self, Command<Message>) {
-    (Self::default(), Command::none())
+  while int.is_err() {
+    int = get_string(prompt).parse::<i128>();
   }
 
-  fn title(&self) -> String {
-    String::new()
-  }
-
-  fn update(&mut self, message: Message, _clipboard: &mut Clipboard) -> Command<Message> {
-    Command::none()
-  }
-
-  fn view(&mut self) -> Element<Message> {
-    Column::new()
-      .padding(60)
-      .push(TextInput::new(
-        &mut self.key_input_state,
-        "",
-        &self.key,
-        |key| {
-          let key = key.parse::<u8>().unwrap();
-
-          if key < 25 && key > 0 {
-            Message::Nothing
-          } else {
-            Message::Nothing
-          }
-        },
-      ))
-      .into()
-  }
+  int.unwrap()
 }
