a80717d8348874a7d39fa31a97885acbe3abaa07
Jay Jackson <jj@jj.is-a.dev>
2021-12-14 20:01:08 +0000

    update

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
 // Trying to decode and encode a caesar cipher with a GUI
 
 // #![windows_subsystem = "windows"]
-// use spellbound::Checker;
-
-const ORIGINAL_ALPHABET: [char; 52] = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-];
+use ryaspeller::{Config, Speller};
 
 fn main() {
-  // let mut spell_checker = Checker::new();
-
-  // println!(
-  //   "{:#?}",
-  //   spell_checker
-  //     .check(&get_input("Type A Sentence:"))
-  //     .map(|error| error.text().to_owned())
-  //     .collect::<Vec<String>>()
-  // );
-
   let menu_options = vec!["Decode", "Encode"];
 
   for (i, item) in menu_options.iter().enumerate() {
     println!("{}: {}", i + 1, item);
   }
 
-  let mut menu_option = get_int(
-    &format!("Choose an option (1-{}): ", menu_options.len()),
-    false,
-  );
+  let mut menu_option = get_int(&format!("Choose an option (1-{}): ", menu_options.len()));
 
   while menu_option as usize > menu_options.len() || menu_option < 0 {
-    menu_option = get_int(
-      &format!("Choose an option (1-{}): ", menu_options.len()),
-      false,
-    )
+    menu_option = get_int(&format!("Choose an option (1-{}): ", menu_options.len()))
   }
 
   let menu_option = menu_options[menu_option as usize - 1];
@@ -48,35 +26,80 @@ fn main() {
 }
 
 fn encode() {
-  let plain_text = get_string("What is the message that you want to encode? ", false);
-  let mut rotation =
-    get_string("What rotation do you want to use (left / right)? ", false).to_lowercase();
+  let plain_text = get_string("What is the message that you want to encode? ");
 
-  while rotation != "left" && rotation != "right" {
-    rotation = get_string("What rotation do you want to use (left / right)? ", false).to_lowercase()
+  let mut shift = get_int("What shift do you want to use (number)? ") as u8;
+
+  while shift >= 25 {
+    shift = get_int("What shift do you want to use (number)? ") as u8
   }
 
-  let mut shift = get_int("What shift do you want to use? ", false) as u8;
-
-  while shift > 25 {
-    shift = get_int("What shift do you want to use? ", false) as u8
-  }
-
-  let alphabet = create_alphabet(shift, &rotation);
-
-  println!("{:#?}", alphabet);
-
-  let mut encoded = plain_text;
-  for (index, letter) in alphabet.iter().enumerate() {
-    encoded = encoded.replace(ORIGINAL_ALPHABET[index], &letter.to_string());
+  let mut encoded = String::new();
+  for letter in plain_text.chars() {
+    if letter.is_uppercase() {
+      encoded.push_str(&(((((letter as u8 + shift) - 65) % 26) + 65) as char).to_string());
+    } else if letter.is_lowercase() {
+      encoded.push_str(&(((((letter as u8 + shift) - 97) % 26) + 97) as char).to_string());
+    } else {
+      encoded.push_str(&letter.to_string());
+    }
   }
 
   println!("{}", encoded);
 }
 
-fn decode() {}
+fn decode() {
+  let knows = get_bool("Do you know the shift (yes / no)? ");
 
-fn _input(prompt: &str) -> String {
+  if knows {
+    let shift = get_int("What is the shift (number)? ") as u8;
+    let encoded = get_string("What is the encoded text? ");
+
+    let mut decoded = String::new();
+    for letter in encoded.chars() {
+      if letter.is_uppercase() {
+        decoded.push_str(&(((((letter as u8 - shift) - 65) % 26) + 65) as char).to_string());
+      } else if letter.is_lowercase() {
+        decoded.push_str(&(((((letter as u8 - shift) - 97) % 26) + 97) as char).to_string());
+      } else {
+        decoded.push_str(&letter.to_string());
+      }
+    }
+
+    println!("{}", decoded);
+  } else {
+    let speller = Speller::new(Config::default());
+    let spelled = speller
+      .check_text("Triky Custle is a funny puzzle game.")
+      .unwrap();
+
+    println!("{:#?}", spelled);
+  }
+}
+
+fn get_bool(prompt: &str) -> bool {
+  let mut _str = get_string(prompt);
+
+  while !_str.to_lowercase().starts_with("y") && !_str.to_lowercase().starts_with("n") {
+    _str = get_string(prompt);
+  }
+
+  _str.to_lowercase().starts_with("y")
+}
+
+fn get_int(prompt: &str) -> isize {
+  let _str = get_string(prompt);
+
+  let mut int = _str.parse::<isize>();
+
+  while int.is_err() {
+    int = get_string(prompt).parse::<isize>();
+  }
+
+  int.unwrap()
+}
+
+fn get_string(prompt: &str) -> String {
   use io::Write;
   use std::io;
 
@@ -89,50 +112,4 @@ fn _input(prompt: &str) -> String {
   io::stdin().read_line(&mut input).ok();
 
   input.trim().to_owned()
-}
-
-fn get_string(prompt: &str, can_be_empty: bool) -> String {
-  let mut input = _input(prompt);
-
-  if !can_be_empty {
-    while input == String::new() {
-      input = _input(prompt)
-    }
-  }
-
-  input
-}
-
-fn get_int(prompt: &str, can_be_empty: bool) -> isize {
-  let _str = get_string(prompt, can_be_empty);
-  let mut int = Ok(0);
-
-  if !(can_be_empty && _str == String::new()) {
-    int = _str.parse::<isize>();
-
-    while int.is_err() {
-      int = get_string(prompt, can_be_empty).parse::<isize>();
-    }
-  }
-
-  int.unwrap()
-}
-
-fn create_alphabet(shift: u8, rotation: &str) -> Vec<char> {
-  let use_mapper = |first_number: u8| {
-    move |code: u8| {
-      (((if rotation == "left" {
-        code - shift
-      } else {
-        code + shift
-      }) % 26)
-        + first_number) as char
-    }
-  };
-
-  let lowercase = (97..122).map(use_mapper(97)).collect::<Vec<char>>();
-
-  let uppercase = (65..90).map(use_mapper(65)).collect::<Vec<char>>();
-
-  [lowercase, uppercase].concat()
 }
