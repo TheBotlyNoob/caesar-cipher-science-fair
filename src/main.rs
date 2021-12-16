b5c32ca1d320ba765d3bcf2f31752bf74f1946ca
Jay Jackson <jj@jj.is-a.dev>
2021-12-16 14:59:00 +0000

    fix big numbers

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-// Trying to decode and encode a caesar cipher with a GUI
+// Trying to decrypt and encrypt a caesar cipher with a GUI
 
 // #![windows_subsystem = "windows"]
-use ryaspeller::{Config, Speller};
 
 fn main() {
-  let menu_options = vec!["Decode", "Encode"];
+  let menu_options = vec!["Decrypt", "Encrypt"];
 
   for (i, item) in menu_options.iter().enumerate() {
     println!("{}: {}", i + 1, item);
@@ -12,68 +11,67 @@ fn main() {
 
   let mut menu_option = get_int(&format!("Choose an option (1-{}): ", menu_options.len()));
 
-  while menu_option as usize > menu_options.len() || menu_option < 0 {
+  while menu_option > menu_options.len() {
     menu_option = get_int(&format!("Choose an option (1-{}): ", menu_options.len()))
   }
 
-  let menu_option = menu_options[menu_option as usize - 1];
+  let menu_option = menu_options[menu_option - 1];
 
   match menu_option {
-    "Decode" => decode(),
-    "Encode" => encode(),
+    "Decrypt" => decrypt(),
+    "Encrypt" => encrypt(),
     &_ => (),
   }
 }
 
-fn encode() {
-  let plain_text = get_string("What is the message that you want to encode? ");
+fn encrypt() {
+  let plain_text = get_string("What is the message that you want to encrypt? ");
 
   let mut shift = get_int("What shift do you want to use (number)? ") as u8;
 
-  while shift >= 25 {
+  while shift > 26 {
     shift = get_int("What shift do you want to use (number)? ") as u8
   }
 
-  let mut encoded = String::new();
+  let mut encrypted = String::new();
   for letter in plain_text.chars() {
     if letter.is_uppercase() {
-      encoded.push_str(&(((((letter as u8 + shift) - 65) % 26) + 65) as char).to_string());
+      encrypted.push_str(&(((((letter as u8 + shift) - 65) % 26) + 65) as char).to_string());
     } else if letter.is_lowercase() {
-      encoded.push_str(&(((((letter as u8 + shift) - 97) % 26) + 97) as char).to_string());
+      encrypted.push_str(&(((((letter as u8 + shift) - 97) % 26) + 97) as char).to_string());
     } else {
-      encoded.push_str(&letter.to_string());
+      encrypted.push_str(&letter.to_string());
     }
   }
 
-  println!("{}", encoded);
+  println!("{}", encrypted);
 }
 
-fn decode() {
+fn decrypt() {
+  let encrypted = get_string("What is the encrypted text? ");
   let knows = get_bool("Do you know the shift (yes / no)? ");
 
   if knows {
-    let shift = get_int("What is the shift (number)? ") as u8;
-    let encoded = get_string("What is the encoded text? ");
+    let mut shift = get_int("What is the shift (number)? ") as u8;
+    while shift > 26 {
+      shift = get_int("What shift do you want to use (number)? ") as u8
+    }
 
-    let mut decoded = String::new();
-    for letter in encoded.chars() {
+    let mut decrypted = String::new();
+    for letter in encrypted.chars() {
       if letter.is_uppercase() {
-        decoded.push_str(&(((((letter as u8 - shift) - 65) % 26) + 65) as char).to_string());
+        decrypted.push_str(&(((((letter as u8 - shift) - 65) % 26) + 65) as char).to_string());
       } else if letter.is_lowercase() {
-        decoded.push_str(&(((((letter as u8 - shift) - 97) % 26) + 97) as char).to_string());
+        decrypted.push_str(
+          &(((((letter as u8 - shift) as i8 - 97).rem_euclid(26)) + 97) as u8 as char).to_string(),
+        );
       } else {
-        decoded.push_str(&letter.to_string());
+        decrypted.push_str(&letter.to_string());
       }
     }
 
-    println!("{}", decoded);
+    println!("{}", decrypted);
   } else {
-    let speller = Speller::new(Config::default());
-    let spelled = speller
-      .check_text("Triky Custle is a funny puzzle game.")
-      .unwrap();
-
-    println!("{:#?}", spelled);
   }
 }
 
@@ -87,13 +85,13 @@ fn get_bool(prompt: &str) -> bool {
   _str.to_lowercase().starts_with("y")
 }
 
-fn get_int(prompt: &str) -> isize {
+fn get_int(prompt: &str) -> usize {
   let _str = get_string(prompt);
 
-  let mut int = _str.parse::<isize>();
+  let mut int = _str.parse::<usize>();
 
   while int.is_err() {
-    int = get_string(prompt).parse::<isize>();
+    int = get_string(prompt).parse::<usize>();
   }
 
   int.unwrap()
