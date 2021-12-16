aa29391dc96311cf7d904570378d95a817eccbbc
Jay Jackson <jj@jj.is-a.dev>
2021-12-16 14:46:00 +0000

    hopefuly get working again

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // Trying to decode and encode a caesar cipher with a GUI
 
 // #![windows_subsystem = "windows"]
-mod dictionary;
+use ryaspeller::{Config, Speller};
 
 fn main() {
   let menu_options = vec!["Decode", "Encode"];
@@ -50,79 +50,41 @@ fn encode() {
 
 fn decode() {
   let knows = get_bool("Do you know the shift (yes / no)? ");
-  let encoded = get_string("What is the encoded text? ");
 
   if knows {
-    let mut shift = get_int("What is the shift (number)? ") as u8;
+    let shift = get_int("What is the shift (number)? ") as u8;
+    let encoded = get_string("What is the encoded text? ");
 
-    while shift >= 25 {
-      shift = get_int("What shift do you want to use (number)? ") as u8
+    let mut decoded = String::new();
+    for letter in encoded.chars() {
+      if letter.is_uppercase() {
+        decoded.push_str(&(((((letter as u8 - shift) - 65) % 26) + 65) as char).to_string());
+      } else if letter.is_lowercase() {
+        decoded.push_str(&(((((letter as u8 - shift) - 97) % 26) + 97) as char).to_string());
+      } else {
+        decoded.push_str(&letter.to_string());
+      }
     }
-
-    let decoded = _decode(shift, &encoded);
 
     println!("{}", decoded);
   } else {
-    use dictionary::DICTIONARY;
-    let mispelled_words_threshold = 25;
+    let speller = Speller::new(Config::default());
+    let spelled = speller
+      .check_text("Triky Custle is a funny puzzle game.")
+      .unwrap();
 
-    let mut decoded = String::new();
-
-    for shift in 0..25 {
-      decoded = _decode(shift, &encoded);
-      let words = decoded.split(' ').collect::<Vec<&str>>();
-
-      let percent_of_mispelled_words = {
-        let mut number_of_mispelled_words = 0;
-        for word in &words {
-          if !DICTIONARY.contains(word) {
-            number_of_mispelled_words += 1;
-          }
-        }
-
-        (100 * number_of_mispelled_words) / words.len()
-      };
-
-      if percent_of_mispelled_words < mispelled_words_threshold {
-        break;
-      } else {
-        continue;
-      };
-    }
-
-    println!("{}", decoded);
+    println!("{:#?}", spelled);
   }
-}
-
-fn _decode(shift: u8, encoded: &str) -> String {
-  let mut decoded = String::new();
-  for letter in encoded.chars() {
-    if letter.is_uppercase() {
-      decoded.push_str(
-        &((((letter as u8 as i8 - shift as i8) - 65).rem_euclid(26) + 65) as u8 as char)
-          .to_string(),
-      );
-    } else if letter.is_lowercase() {
-      decoded.push_str(
-        &((((letter as u8 as i8 - shift as i8) - 65).rem_euclid(26) + 97) as u8 as char)
-          .to_string(),
-      );
-    } else {
-      decoded.push_str(&letter.to_string());
-    }
-  }
-
-  decoded
 }
 
 fn get_bool(prompt: &str) -> bool {
   let mut _str = get_string(prompt);
 
-  while !_str.to_lowercase().starts_with('y') && !_str.to_lowercase().starts_with('n') {
+  while !_str.to_lowercase().starts_with("y") && !_str.to_lowercase().starts_with("n") {
     _str = get_string(prompt);
   }
 
-  _str.to_lowercase().starts_with('y')
+  _str.to_lowercase().starts_with("y")
 }
 
 fn get_int(prompt: &str) -> isize {
